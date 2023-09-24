efc272b9f522c1bdc5410ccae5a7d8d408408def
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-24 10:20:26 -0700

    models for the api

---
+++ Models/Country.cs
@@ -0,0 +1,12 @@
+public class Country 
+{
+    public string? Rank { get; set; }
+    public string? CCA3 { get; set; }
+    public string? Name { get; set; }
+    public string? Capital { get; set; }
+    public Population? Population { get; set; }
+    public string? Area { get; set; }
+    public string? Density { get; set; }
+    public string? GrowthRate { get; set; }
+    public string? WorldPopulationPercentage { get; set; }
+}
---
+++ Models/Population.cs
@@ -0,0 +1,11 @@
+public class Population 
+{
+    public string? PopulationIn2022 { get; set; }
+    public string? PopulationIn2020 { get; set; }
+    public string? PopulationIn2015 { get; set; }
+    public string? PopulationIn2010 { get; set; }
+    public string? PopulationIn2000 { get; set; }
+    public string? PopulationIn1990 { get; set; }
+    public string? PopulationIn1980 { get; set; }
+    public string? PopulationIn1970 { get; set; }
+}
